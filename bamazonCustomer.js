c75a3e40843b53de69338f65ba8f063521b0a465
Dawn Sallas <dawndesignerd@Dawns-MBP-2.hsd1.va.comcast.net>
2019-10-04 21:48:32 -0400

    i have added alot to the commit, i struggled most with the dislaping of the table and getting the responses to work with a parameter, but i was able to find help on the world wide web YAY. also i will have readme updated asap

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,21 +1,104 @@
-var mysql = require("mysql");
+var mysql = require("mysql");;
+var inquirer = require("inquirer");
+
+
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Freebird711612",
-    database: "bamazonDB"
-  });
-  
-  connection.connect(function(err) {
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazonDB"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("\nWelcome Welcome!Shop here now!\n");
+
+  allProducts();
+});
+
+function allProducts() {
+  // query the database for all items for sale
+  connection.query("SELECT * from products;", function (err, results, fields) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-  });
-  function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
-    });
+    else {
+      console.table(results);
+    }
+    pickProduct();
+
   }
+
+  )
+}
+
+function pickProduct() {
+  inquirer
+    .prompt([
+      {
+        name: "product",
+        type: "input",
+        message: "What is the item_id of the product you would like to buy?"
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to buy?"
+      }
+    ])
+    .then(function (answer) {
+
+
+      var product = answer.product;
+      var quantity = answer.quantity;
+
+      var queryProducts = "SELECT * FROM products WHERE ?";
+      var cost
+      connection.query(queryProducts, { item_id: product }, function (err, res) {
+        var productInfo = res[0];
+        if (err) throw err;
+        if (quantity > productInfo.stock_quantity) {
+          console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+          console.log("I'm sorry we don't have enough in stock, choose a smaller quantity!");
+          console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+          allProducts()
+        }
+        else {
+
+          if (quantity <= productInfo.stock_quantity) {
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            console.log("We have " + quantity + " " + productInfo.product_name + "s in stock for your order!")
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            console.log("Thank you for your order! Please wait while we process your order!");
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+          }
+          if (cost = quantity * productInfo.price) {
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            console.log("The total cost of your order is $" + cost);
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+          }
+          var queryUpdate = "UPDATE products SET ? WHERE ?"
+          connection.query(queryUpdate, [{ stock_quantity: answer.quantity }, { item_id: product }], function (err, res) {
+            if (err) throw err;
+            else {
+              console.log("Yo Stuff Has Been Added Fool!");
+              inquirer
+                .prompt({
+                  name: 'next',
+                  type: "input",
+                  message: 'Would you like to place another order (Yes/No)?',
+                })
+                .then(function (answer) {
+                  if (answer.next === "Yes") {
+                    allProducts();
+                  } else {
+                    connection.end()
+                    console.log("Thank You! Come Agian!");
+                  }
+                });
+            }
+          });
+        }
+      });
+    });
+}
